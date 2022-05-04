333e08c1d8121f1914022efbfd7c7350006589b9
Rasujon3 <rasujon3@gmail.com>
2022-05-04 16:36:28 +0600

    sells data load from real server

--- src/hooks/useData.js
+++ src/hooks/useData.js
@@ -3,7 +3,7 @@ import { useEffect, useState } from "react";
 const useData = () => {
   const [data, setData] = useState([]);
   useEffect(() => {
-    const url = `data.json`;
+    const url = `https://assignmenteleven.herokuapp.com/sells`;
     fetch(url)
       .then((res) => res.json())
       .then((data) => setData(data));
